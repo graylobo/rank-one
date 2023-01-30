123fffa785246e80af87304808ab510316c1c9e8
graylobo <cazador.min@gmail.com>
2023-01-30 23:59:05 +0900

    more size 수정

--- src/components/navigation/SideBar.tsx
+++ src/components/navigation/SideBar.tsx
@@ -7,8 +7,8 @@ import styled from "styled-components";
 const Wrapper = styled.div`
   position: relative;
   #more {
-    width: 1vw;
-    height: 1vw;
+    width: 25px;
+    height: 25px;
     cursor: pointer;
   }
 `;
