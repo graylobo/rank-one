2b8f264c740d843142b318f87831f7c456cbd49f
graylobo <cazador.min@gmail.com>
2023-02-07 19:23:17 +0900

    next-seo install

--- src/pages/ranking/royal/watch.tsx
+++ src/pages/ranking/royal/watch.tsx
@@ -1,6 +1,13 @@
+import { NextSeo } from "next-seo";
+import Head from "next/head";
 import React from "react";
 import Watch from "../../../components/ranking/royal/Watch";
 
 export default function watch() {
-  return <Watch />;
+  return (
+    <>
+      <NextSeo title="" />
+      <Watch />
+    </>
+  );
 }
